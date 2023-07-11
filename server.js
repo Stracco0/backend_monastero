1e190a0f5bfd6428f7ce1972dd4253be476807bf
Stracco0 <133965736+Stracco0@users.noreply.github.com>
2023-07-11 11:30:56 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ async function Collegamento(){
     while (true) {
         const MymongoClient=new MongoClient("mongodb+srv://admin:PjxuwGMAlkPGzAJX@servermongodbprovamonas.xqbrdmt.mongodb.net/ProvaMonasteroStella?retryWrites=true&w=majority")
         const data = await MymongoClient.db().collection("Eventi").find().toArray()
+        app.get("/",(re,res)=>{
+            return res.json("Backend")
+        })
+        app.get("/Ottieni_data",(req,res,next)=>{
+            return res.json(data)
+        })
+        app.listen(8081,()=>{
+            console.log("listening")
+        })
         await delay(20000)
     }
-    app.get("/",(re,res)=>{
-        return res.json("Backend")
-    })
-    app.get("/Ottieni_data",(req,res,next)=>{
-        return res.json(data)
-    })
-    app.listen(8081,()=>{
-        console.log("listening")
-    })
 }
 Collegamento()
