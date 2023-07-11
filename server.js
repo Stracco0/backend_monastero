576858f172ad76102a77e1f051a6b3bc02fd7eee
Stracco0 <133965736+Stracco0@users.noreply.github.com>
2023-07-11 11:44:59 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 const cors = require('cors')
 const express = require("express")
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { restart } = require('nodemon');
+const { reset } = require('nodemon/lib/utils');
 const app = express()
 app.use(cors())
 async function Collegamento(){
-    while (true) {
-        const MymongoClient=new MongoClient("mongodb+srv://admin:PjxuwGMAlkPGzAJX@servermongodbprovamonas.xqbrdmt.mongodb.net/ProvaMonasteroStella?retryWrites=true&w=majority")
-        const data = await MymongoClient.db().collection("Eventi").find().toArray()
-        app.get("/",(re,res)=>{
-            return res.json("Backend")
-        })
-        app.get("/Ottieni_data",(req,res,next)=>{
-            return res.json(data)
-        })
-        app.listen(8081,()=>{
-            console.log("listening")
-        })
-        await delay(20000)
-    }
+    const MymongoClient=new MongoClient("mongodb+srv://admin:PjxuwGMAlkPGzAJX@servermongodbprovamonas.xqbrdmt.mongodb.net/ProvaMonasteroStella?retryWrites=true&w=majority")
+    const data = await MymongoClient.db().collection("Eventi").find().toArray()
+    app.get("/",(re,res)=>{
+        return res.json("Backend")
+    })
+    app.get("/Ottieni_data",(req,res,next)=>{
+        return res.json(data)
+    })
+    app.listen(8081,()=>{
+        console.log("listening")
+    })
 }
 Collegamento()
