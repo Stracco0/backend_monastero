80baad69cf674be9e37df899bcd57ef2037de8fa
Stracco0 <133965736+Stracco0@users.noreply.github.com>
2023-07-11 11:15:58 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const cors = require('cors')
-app.use(cors())
-app.get('/products/:id', function (req, res, next) {
-    res.json({msg: 'This is CORS-enabled for all origins!'})
-  })
-  
-  app.listen(80, function () {
-    console.log('CORS-enabled web server listening on port 80')
-  })
 const express = require("express")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
+app.use(cors())
 async function Collegamento(){
     const MymongoClient=new MongoClient("mongodb+srv://admin:PjxuwGMAlkPGzAJX@servermongodbprovamonas.xqbrdmt.mongodb.net/ProvaMonasteroStella?retryWrites=true&w=majority")
     const data = await MymongoClient.db().collection("Eventi").find().toArray()
