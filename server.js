8c4e53387a5b46afbe12f2eaf1d8b3735133b238
Stracco0 <133965736+Stracco0@users.noreply.github.com>
2023-07-11 10:10:08 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+async function Collegamento(){
+    const MymongoClient=new MongoClient("mongodb+srv://admin:PjxuwGMAlkPGzAJX@servermongodbprovamonas.xqbrdmt.mongodb.net/ProvaMonasteroStella?retryWrites=true&w=majority")
+    const data = await MymongoClient.db().collection("Eventi").find().toArray()
+    app.get("/",(re,res)=>{
+        return res.json("Backend")
+    })
+    app.get("/Ottieni_data",(req,res)=>{
+        return res.json(data)
+    })
+    app.listen(8081,()=>{
+        console.log("listening")
+    })
+}
+Collegamento()
