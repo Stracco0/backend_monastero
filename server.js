cb81ec9b71254de3afd5c6af0a57f71fc4be9cb6
Stracco0 <133965736+Stracco0@users.noreply.github.com>
2023-07-11 11:14:39 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+const cors = require('cors')
+app.use(cors())
+app.get('/products/:id', function (req, res, next) {
+    res.json({msg: 'This is CORS-enabled for all origins!'})
+  })
+  
+  app.listen(80, function () {
+    console.log('CORS-enabled web server listening on port 80')
+  })
 const express = require("express")
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
@@ -7,7 +16,7 @@ async function Collegamento(){
     app.get("/",(re,res)=>{
         return res.json("Backend")
     })
-    app.get("/Ottieni_data",(req,res)=>{
+    app.get("/Ottieni_data",(req,res,next)=>{
         return res.json(data)
     })
     app.listen(8081,()=>{
